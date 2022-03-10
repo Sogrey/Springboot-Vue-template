d25e33110e05fe85008dbb3b46b94b19f97b89a7
sogrey <suzhaoheng2006@163.com>
2022-03-11 01:33:07 +0800

    💖 deploy main branch.

---
+++ SpringbootService/src/main/java/top/sogrey/springbootservice/SpringbootMainApplication.java
@@ -0,0 +1,16 @@
+package top.sogrey.springbootservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication // Spring Boot项目的核心注解，主要目的是开启自动配置
+public class SpringbootMainApplication {
+
+	// 在main方法中启动一个应用，即：这个应用的入口
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMainApplication.class, args);
+	}
+}
---
+++ SpringbootService/src/main/java/top/sogrey/springbootservice/controller/RouteController.java
@@ -0,0 +1,21 @@
+package top.sogrey.springbootservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // 标明这是一个SpringMVC的Controller控制器
+@RequestMapping(value = "/api")
+public class RouteController {
+	@RequestMapping("/")
+	@ResponseBody
+	public String hello() {
+		return "hello world";
+	}
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String sayHello(String name) {
+		return "hello " + name;
+	}
+}
